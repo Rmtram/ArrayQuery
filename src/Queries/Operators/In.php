3df37bed59c218ca4c41d631397681b2bb09a24c
Rmtram <rmtram95@gmail.com>
2019-12-20 01:32:18 +0900

    :sparkles: add operations: in, notIn

---
+++ src/Queries/Operators/In.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rmtram\ArrayQuery\Queries\Operators;
+
+/**
+ * Class In
+ * @package Rmtram\ArrayQuery\Queries\Operators
+ */
+class In extends AbstractComparison
+{
+    /**
+     * @var string
+     */
+    protected $operator = 'in';
+}
---
+++ src/Queries/Operators/NotIn.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rmtram\ArrayQuery\Queries\Operators;
+
+/**
+ * Class NotIn
+ * @package Rmtram\ArrayQuery\Queries\Operators
+ */
+class NotIn extends AbstractComparison
+{
+    /**
+     * @var string
+     */
+    protected $operator = '!in';
+}
