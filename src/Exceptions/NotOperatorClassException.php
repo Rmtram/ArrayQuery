3ecb37dcccfae0541b1a7ea9b4e70632d14d26a1
Rmtram <rmtram95@gmail.com>
2019-12-20 01:25:43 +0900

    :shower: Remove unnecessary files.

--- src/Exceptions/NotOperatorClassException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rmtram\ArrayQuery\Exceptions;
-
-/**
- * Class NotOperatorClassException
- * @package Rmtram\ArrayQuery\Exceptions
- */
-class NotOperatorClassException extends BaseException {}
--- src/Exceptions/UndefinedOperatorClassException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rmtram\ArrayQuery\Exceptions;
-
-/**
- * Class UndefinedOperatorClassException
- * @package Rmtram\ArrayQuery\Exceptions
- */
-class UndefinedOperatorClassException extends BaseException {}
